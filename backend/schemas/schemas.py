72f00f701ca326c44327ad2234b1ffbe3f75c308
Naqiuddin Kamil <naqiu923@gmail.com>
2025-07-01 17:29:55 +0800

    Adding the Pydantic schemas inside schemas/schemas.py

---
+++ backend/schemas/schemas.py
@@ -0,0 +1,52 @@
+from pydantic import BaseModel
+from typing import Optional, List
+from datetime import datetime
+
+# --- Pydantic Schemas for Furniture ---
+
+# Pydantic Schema: Represents the structure of a furniture item
+
+class FurnitureCreate(BaseModel):
+  furniture_id: Optional[str] = None
+  style: Optional[str] = None
+  room: Optional[str] = None
+  name: Optional[str] = None
+  type: Optional[str] = None
+  price: Optional[float] = None
+  image_link: Optional[str] = None
+  purchase_link: Optional[str] = None
+
+  class Config:
+    orm_mode = True # Allows Pydantic to work with SQLAlchemy models directly
+
+class FurnitureModel(FurnitureCreate):
+  id: int
+
+# --- Pydantic Schemas for Generated Rooms ---
+
+# Pydantic Schema: Represents the structure of a generated room
+class GeneratedRoomModel(BaseModel):
+  id: int
+  original_image_path: str
+  generated_image_path: str
+  generated_room_id: int
+  room_style: Optional[str] = None
+  design_style: Optional[str] = None
+  generated_date: Optional[datetime] = None
+
+  class Config:
+    orm_mode = True # Allows Pydantic to work with SQLAlchemy models directly
+
+
+# --- Pydantic Schemas for Furniture Coordinates ---
+
+# Pydantic Schema: Represents the structure of furniture coordinates
+class FurnitureCoordinatesModel(BaseModel):
+  id: Optional[int] = None
+  generated_room_id: int
+  furniture_id: str
+  x_coordinate: float
+  y_coordinate: float
+
+class Config:
+  orm_mode = True # Allows Pydantic to work with SQLAlchemy models directly
